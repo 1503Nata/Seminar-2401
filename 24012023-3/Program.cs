b02b803571cd093f41a5ff5c6760635c8eede1b6
Natalya <pauls88@yandex.ru>
2023-01-27 20:40:00 +0500

    Addfile24012023-3

---
+++ 24012023-3/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine();
+Console.WriteLine("Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.");
+Console.WriteLine();
+int[] massiv = new int[8];
+void FillArray(int[] massiv)
+    {
+    Console.WriteLine($"Формируем массив из {massiv.Length} элементов");
+    for (int i = 0; i < massiv.Length; i++) 
+    {
+    massiv[i] = new Random().Next(-11,20);
+    }
+    return;
+    }
+FillArray(massiv);
+
+void Print(int[] massiv)
+    {
+    int i = 0;
+    Console.Write($"Итоговый массив: [ {massiv[i]}");
+    for (i = 1; i < massiv.Length; i++)
+    {
+    Console.Write($", {massiv[i]}");
+    }
+    Console.Write(" ]");
+    return;
+    }
+
+Print(massiv);
+
